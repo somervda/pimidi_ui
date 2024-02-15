4aa7150a4c0fcee92f6c661143b5dc6be9de0833
David SurfacePro <somervda@comcast.net>
2024-02-15 17:37:13 -0500

    wip - added components and tabs. Also made a favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SettingsComponent } from './settings/settings.component';
+import { AiComponent} from './ai/ai.component';
+import { SequencerComponent } from './sequencer/sequencer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatTabsModule,
+    SettingsComponent,
+    AiComponent,
+    SequencerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/sequencer/sequencer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sequencer',
+  standalone: true,
+  imports: [],
+  templateUrl: './sequencer.component.html',
+  styleUrl: './sequencer.component.scss'
+})
+export class SequencerComponent {
+
+}

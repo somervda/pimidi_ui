110e6f26be0ccee53f5b1450a1027a1b4bb06eda
David SurfacePro <somervda@comcast.net>
2024-02-18 12:45:14 -0500

    Wip - settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,MatCardModule
+],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  settingsForm: FormGroup;
+  settings: undefined;
+  constructor(private fb: FormBuilder) {
+    this.settingsForm = this.fb.group({
+      pumpOnSeconds: [
+        this.settings?.pumpOnSeconds,
+        [
+          Validators.required,
+          Validators.min(30),
+          Validators.max(500),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ])
+
+   }
 
 }

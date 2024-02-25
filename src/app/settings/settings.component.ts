7a5856fd318842dedada042b23af9b48e6b9fc9c
David <39265971+somervda@users.noreply.github.com>
2024-02-25 09:05:12 -0500

    Wip - formbuilder and service

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -3,16 +3,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-
+import { SettingService } from '../services/setting.service';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [ReactiveFormsModule, MatCardModule, MatCardModule, MatInputModule
-  ],
+  imports: [ReactiveFormsModule, MatCardModule, MatCardModule, MatInputModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
   settingsForm: FormGroup;
@@ -22,28 +20,16 @@ export class SettingsComponent {
   midiDisplay: boolean = true;
   midiDefaultChannel: number = 1;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private settingService: SettingService) {
     this.settingsForm = this.fb.group({
       cvMaxVolts: [
         this.cvMaxVolts,
-        [
-          Validators.required,
-          Validators.min(0),
-          Validators.max(12)
-        ]
-      ]
-    },
-      {
-        cvMidiChannel: [
-          this.cvMidiChannel,
-          [
-            Validators.required,
-            Validators.min(1),
-            Validators.max(16)
-          ]
-        ]
-      })
-
+        [Validators.required, Validators.min(0), Validators.max(12)],
+      ],
+      cvMidiChannel: [
+        this.cvMidiChannel,
+        [Validators.required, Validators.min(1), Validators.max(16)],
+      ],
+    });
   }
-
 }

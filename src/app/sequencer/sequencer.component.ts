c716bce134231ec77d1c886c7ba5bf8acb041623
David <39265971+somervda@users.noreply.github.com>
2024-02-28 13:39:37 -0500

    wip-sequencer

--- src/app/sequencer/sequencer.component.ts
+++ src/app/sequencer/sequencer.component.ts
@@ -15,25 +15,16 @@ export class SequencerComponent {
   noteOn$$: Subscription | undefined;
   noteOff$$: Subscription | undefined;
   midiReset$$: Subscription | undefined;
-  playingNote: number = -1;
 
   constructor(private playerService: PlayerService) {}
 
   noteOn(event: Event): void {
-    let midiNote = parseInt((event.target as Element).id) + 50;
-    if (this.playingNote != -1) {
-      // If a note is still playing turn it off
-      this.noteOff$$ = this.playerService
-        .midiNoteOff(this.playingNote)
-        .subscribe(() => {});
-      this.playingNote = -1;
-    }
+    let midiNote = parseInt((event.target as Element).id) + 40;
     console.log('Note On:', midiNote);
     this.noteOn$$ = this.playerService.midiNoteOn(midiNote).subscribe(() => {});
-    this.playingNote = midiNote;
   }
   noteOff(event: Event): void {
-    let midiNote = parseInt((event.target as Element).id) + 50;
+    let midiNote = parseInt((event.target as Element).id) + 40;
     console.log('Note Off:', midiNote);
     this.noteOff$$ = this.playerService
       .midiNoteOff(midiNote)
